ba762d6462f280e3478a4b613dc187c2fb011c41
gustavo.gr697.gm <gustavo.gr697.gm@gmail.com>
2025-07-21 02:47:20 +0000

    botoões de login com google, esqueci minha senha e registro feitos

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:myapp/screen/forgot_password_screen.dart';
+import 'package:myapp/screen/register_screen.dart';
+import 'package:sign_in_button/sign_in_button.dart';
 
 bool obscure = true;
 
@@ -32,97 +35,94 @@ class Login extends StatelessWidget {
                 ),
               ),
 
-              const SizedBox(height: 10),
-
-              // Texto "Bem-vindo de volta!" com contorno
-              Stack(
-                children: <Widget>[
-                  Text(
-                    'Bem-vindo de volta!',
-                    style: TextStyle(
-                      fontFamily: "Gilroy",
-                      fontSize: 32,
-                      foreground:
-                          Paint()
-                            ..style = PaintingStyle.stroke
-                            ..strokeWidth = 4
-                            ..color = Colors.white,
-                    ),
-                  ),
-                  Text(
-                    'Bem-vindo de volta!',
-                    style: TextStyle(
-                      fontFamily: "Gilroy",
-                      fontSize: 32,
-                      color: Color(0xFF004D40),
-                    ),
-                  ),
-                ],
-              ),
-
+              BuildStack(),
               const SizedBox(height: 8),
-
-              // Subtexto descritivo
-              Text(
-                'Organize sua rotina com facilidade.',
-                style: TextStyle(
-                  fontFamily: "Gilroy",
-                  fontSize: 16,
-                  color: Colors.white.withOpacity(0.8),
-                ),
-              ),
-
-              Padding(
-                padding: EdgeInsets.fromLTRB(40, 40, 40, 8),
-                child: TextFormField(
-                  style: TextStyle(
-                    color: Colors.white, // cor do texto digitado
-                    fontFamily: "Gilroy",
-                  ),
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.email, color: Colors.white),
-                    border: OutlineInputBorder(),
-                    labelText: "Email",
-                    hintText: "Email",
-                    hintStyle: TextStyle(
-                      color: Colors.white,
-                      fontFamily: "Gilroy",
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(40, 0, 40, 20),
-                child: PassWordField(),
-              ),
-
-              Padding(
-                padding: EdgeInsets.fromLTRB(40, 10, 40, 20),
-                child: ButtonTheme(
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: Text(
-                      'Entrar',
-                      style: TextStyle(
-                        color: Color(0xFF004D40),
-                        fontFamily: "Gilroy",
-                        fontSize: 20,
-                        fontWeight: FontWeight.w900,
-                      ),
-                    ),
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: const Size(200, 50), // LARGURA x ALTURA,
-                      side: BorderSide(
-                        style: BorderStyle.solid,
-                        color: Color(0xFF004D40),
-                        width: 2,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
+              BuildText(),
+              EmailField(),
+              PassWordField(),
+              ForgotPassword(),
+              ButtonEntrar(),
+              LoginWithGoogle(),
+              GoToRegister(),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class BuildText extends StatelessWidget {
+  const BuildText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return // Subtexto descritivo
+    Text(
+      'Organize sua rotina com facilidade.',
+      style: TextStyle(
+        fontFamily: "Gilroy",
+        fontSize: 16,
+        color: Colors.white.withOpacity(0.8),
+      ),
+    );
+  }
+}
+
+class BuildStack extends StatelessWidget {
+  const BuildStack({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return // Texto "Bem-vindo de volta!" com contorno
+    Stack(
+      children: <Widget>[
+        Text(
+          'Bem-vindo de volta!',
+          style: TextStyle(
+            fontFamily: "Gilroy",
+            fontSize: 32,
+            foreground:
+                Paint()
+                  ..style = PaintingStyle.stroke
+                  ..strokeWidth = 4
+                  ..color = Colors.white,
+          ),
+        ),
+        Text(
+          'Bem-vindo de volta!',
+          style: TextStyle(
+            fontFamily: "Gilroy",
+            fontSize: 32,
+            color: Color(0xFF004D40),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class EmailField extends StatelessWidget {
+  const EmailField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Padding(
+      padding: EdgeInsets.fromLTRB(40, 40, 40, 8),
+      child: TextFormField(
+        style: TextStyle(
+          color: Colors.white, // cor do texto digitado
+          fontFamily: "Gilroy",
+        ),
+        decoration: InputDecoration(
+          prefixIcon: Icon(Icons.email, color: Colors.white),
+          border: OutlineInputBorder(),
+          labelText: "Email",
+          hintText: "Email",
+          hintStyle: TextStyle(color: Colors.white, fontFamily: "Gilroy"),
         ),
       ),
     );
@@ -139,39 +139,185 @@ class PassWordField extends StatefulWidget {
 class _PasswordFieldState extends State<PassWordField> {
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      obscureText: obscure,
-      style: const TextStyle(color: Colors.white, fontFamily: "Gilroy"),
-      decoration: InputDecoration(
-        prefixIcon: Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: SvgPicture.asset(
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(40, 0, 40, 20),
+      child: TextFormField(
+        obscureText: obscure,
+        style: const TextStyle(color: Colors.white, fontFamily: "Gilroy"),
+        decoration: InputDecoration(
+          prefixIcon: SvgPicture.asset(
             'assets/icons/password-svgrepo-com.svg',
             colorFilter: const ColorFilter.mode(Colors.white, BlendMode.srcIn),
             width: 24,
             height: 24,
+            fit: BoxFit.scaleDown,
           ),
-        ),
-        suffixIcon: IconButton(
-          onPressed: () {
-            setState(() {
-              obscure = !obscure;
-            });
-          },
-          icon: SvgPicture.asset(
-            obscure
-                ? 'assets/icons/eye-password-see-view-svgrepo-com.svg' //se for verdadeiro
-                : 'assets/icons/eye-password-show-svgrepo-com.svg', //se for falso
-            colorFilter: const ColorFilter.mode(Colors.white, BlendMode.srcIn),
-            width: 24,
-            height: 24,
+          suffixIcon: IconButton(
+            onPressed: () {
+              setState(() {
+                obscure = !obscure;
+              });
+            },
+            icon: SvgPicture.asset(
+              obscure
+                  ? 'assets/icons/eye-password-see-view-svgrepo-com.svg'
+                  : 'assets/icons/eye-password-show-svgrepo-com.svg',
+              colorFilter: const ColorFilter.mode(
+                Colors.white,
+                BlendMode.srcIn,
+              ),
+              width: 24,
+              height: 24,
+            ),
           ),
+          border: const OutlineInputBorder(),
+          labelText: "Senha",
+          hintText: "Senha",
+          hintStyle: const TextStyle(color: Colors.white, fontFamily: "Gilroy"),
         ),
-        border: const OutlineInputBorder(),
-        labelText: "Senha",
-        hintText: "Senha",
-        hintStyle: const TextStyle(color: Colors.white, fontFamily: "Gilroy"),
       ),
     );
   }
+}
+
+class ForgotPassword extends StatelessWidget {
+  const ForgotPassword({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(40, 0, 40, 20),
+        child: InkWell(
+        onTap: () {
+          Navigator.of(context).pushReplacement(_createRoute_ForgotPassword());
+        },
+        child: new Text(
+          "Esqueci minha senha",
+          style: TextStyle(
+            color: Colors.azulGaivota,
+            fontWeight: FontWeight.w900,
+          ),
+        ),
+      ), )
+    );
+  }
+}
+
+class ButtonEntrar extends StatelessWidget {
+  const ButtonEntrar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Padding(
+      padding: EdgeInsets.fromLTRB(40, 10, 40, 20),
+      child: ButtonTheme(
+        child: ElevatedButton(
+          onPressed: () {},
+          child: Text(
+            'Entrar',
+            style: TextStyle(
+              color: Color(0xFF004D40),
+              fontFamily: "Gilroy",
+              fontSize: 20,
+              fontWeight: FontWeight.w900,
+            ),
+          ),
+          style: ElevatedButton.styleFrom(
+            minimumSize: const Size(200, 50), // LARGURA x ALTURA,
+            side: BorderSide(
+              style: BorderStyle.solid,
+              color: Color(0xFF004D40),
+              width: 2,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class LoginWithGoogle extends StatelessWidget {
+  const LoginWithGoogle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(100, 5, 100, 20),
+      child: SizedBox(
+        width: double.infinity,
+        child: SignInButton(
+          Buttons.google,
+          text: "Logar com Google",
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
+}
+
+class GoToRegister extends StatelessWidget {
+  const GoToRegister({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Center(
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Text(
+            "Não tem uma conta? ",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.w900),
+          ),
+          InkWell(
+            onTap: () {
+              Navigator.of(context).pushReplacement(_createRoute_Register());
+            },
+            child: new Text(
+              "Registre-se",
+              style: TextStyle(
+                color: Colors.azulGaivota,
+                fontWeight: FontWeight.w900,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+Route _createRoute_Register() {
+  return PageRouteBuilder(
+    pageBuilder:
+        (context, animation, secondaryAnimation) => const RegisterScreen(),
+    transitionsBuilder: (_, animation, __, child) {
+      final curved = CurvedAnimation(
+        parent: animation,
+        curve: Curves.easeInOutCubic,
+      );
+      return FadeTransition(opacity: curved, child: child);
+    },
+  );
+}
+
+Route _createRoute_ForgotPassword() {
+  return PageRouteBuilder(
+    pageBuilder:
+        (context, animation, secondaryAnimation) =>
+            const ForgotPasswordScreen(),
+    transitionsBuilder: (_, animation, __, child) {
+      final curved = CurvedAnimation(
+        parent: animation,
+        curve: Curves.easeInOutCubic,
+      );
+      return FadeTransition(opacity: curved, child: child);
+    },
+  );
 }
---
+++ lib/screen/register_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatelessWidget
+{
+  const RegisterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+        body: Center(
+          child: Text('Tela de Cadastro'),
+        ),
+    );
+  }
+}
